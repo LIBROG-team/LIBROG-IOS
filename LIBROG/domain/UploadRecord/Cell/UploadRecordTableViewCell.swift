c29c42202462e23c50b800dc9aa750ee34ab8380
gom1n <alice436777@gmail.com>
2022-07-09 07:12:56 +0900

    [feat] Make uploadRecord page & tableView & completeButton

---
+++ LIBROG/domain/UploadRecord/Cell/UploadRecordTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UplodaRecordTableViewCell.swift
+//  LIBROG
+//
+//  Created by gomin on 2022/07/09.
+//
+
+import UIKit
+
+class UplodaRecordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ LIBROG/domain/UploadRecord/UploadRecordViewController.swift
@@ -0,0 +1,45 @@
+//
+//  UploadRecordViewController.swift
+//  LIBROG
+//
+//  Created by gomin on 2022/07/09.
+//
+
+import UIKit
+
+class UploadRecordViewController: UIViewController {
+    @IBOutlet weak var uploadRecordTableView: UITableView!
+    @IBOutlet weak var completeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        uploadRecordTableView.delegate = self
+        uploadRecordTableView.dataSource = self
+        
+        uploadRecordTableView.separatorStyle = .none
+        
+        let uploadRecordNib = UINib(nibName: "UploadRecordTableViewCell", bundle: nil)
+        uploadRecordTableView.register(uploadRecordNib, forCellReuseIdentifier: "UploadRecordTableViewCell")
+    }
+}
+extension UploadRecordViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UploadRecordTableViewCell", for: indexPath) as? UplodaRecordTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 667
+    }
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard cell is MainFlowerTableViewCell else {
+            return
+        }
+    }
+}
