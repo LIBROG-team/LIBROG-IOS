933c5ff8174a5424cb279754d5553e398ecdba11
theunghee02 <20211150@sungshin.ac.kr>
2022-07-12 21:08:13 +0900

    [file] Rename cell

---
+++ LIBROG/domain/Upload/Cell/InputTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InputTableViewCell.swift
+//  LIBROG
+//
+//  Created by 최승희 on 2022/07/12.
+//
+
+import UIKit
+
+class InputTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
