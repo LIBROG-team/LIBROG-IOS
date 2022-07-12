f8866ad6eb5ccf741c5e8858c2e43b13ad40a226
theunghee <20211150@sungshin.ac.kr>
2022-07-12 20:52:10 +0900

    [fix] searchBar & tableView

--- LIBROG/domain/Upload/UploadViewController.swift
+++ LIBROG/domain/Upload/UploadViewController.swift
@@ -7,20 +7,56 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
-    @IBOutlet weak var searchBar: UISearchBar!
+class UploadViewController: UITableViewController , UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate{
+    //create the search controller and result contoller
+    var dataArray = [Data]()
+    var fileteredData = [Data]()
+    
+    var searchController = UISearchController()
+    var resultVC = UITableViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setData()
+        searchController = UISearchController(searchResultsController: resultVC)
         
-        searchBar.delegate = self
+        tableView.tableHeaderView = searchController.searchBar
+        //usally good to set the presentation context
+        self.definesPresentationContext = true
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.delegate = self
         
-        let searchBarImage = UIImage()
-        searchBar.backgroundImage = searchBarImage
-        // Do any additional setup after loading the view.
+        resultVC.tableView.delegate = self
+        resultVC.tableView.dataSource = self
     }
-}
 
-extension UploadViewController: UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        fileteredData = dataArray.filter({ (data:Data) -> Bool in
+            return data.main.lowercased().contains(searchController.searchBar.text!.lowercased())
+        })
+        resultVC.tableView.reloadData()
+    }
     
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableView == resultVC.tableView ? fileteredData.count : dataArray.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = (tableView == resultVC.tableView ? fileteredData[indexPath.row].main : dataArray[indexPath.row].main)
+        return cell
+    }
+    
+    private func setData(){
+        dataArray.append(Data(main: "One", detail: .A))
+        dataArray.append(Data(main: "Two", detail: .A))
+        dataArray.append(Data(main: "Three", detail: .A))
+        dataArray.append(Data(main: "Ten", detail: .B))
+        dataArray.append(Data(main: "Eleven", detail: .B))
+        dataArray.append(Data(main: "Twelve", detail: .B))
+    }
+
 }
