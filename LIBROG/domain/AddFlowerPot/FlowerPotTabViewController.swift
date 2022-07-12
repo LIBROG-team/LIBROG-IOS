1c65a9ac2b8a52474c0f9bdcac9c63542051f1fa
gom1n <alice436777@gmail.com>
2022-07-12 17:58:54 +0900

    [style] Custom tabManBar

--- LIBROG/domain/AddFlowerPot/FlowerPotTabViewController.swift
+++ LIBROG/domain/AddFlowerPot/FlowerPotTabViewController.swift
@@ -24,21 +24,36 @@ class FlowerPotTabViewController: TabmanViewController {
         
         self.dataSource = self
 
-        // Create bar
+        // 바 생성 + tabbar 에 관한 디자인처리를 여기서 하면 된다.
         let bar = TMBar.ButtonBar()
         bar.layout.transitionStyle = .snap // Customize
-
-        // Add to view
-        addBar(bar, dataSource: self, at: .top)
+        // tab 밑 bar 색깔 & 크기
+        bar.indicator.weight = .custom(value: 1)
+        bar.indicator.tintColor = .black
+        // tap center
+        bar.layout.alignment = .centerDistributed
+        // tap 사이 간격
+        bar.layout.interButtonSpacing = 114
+        bar.indicator.tintColor = UIColor(named: "LIBROGColor")
+        bar.indicator.overscrollBehavior = .bounce
+        bar.backgroundView.style = .flat(color: .white)
+        // tap 선택 / 미선택
+        bar.buttons.customize { (button) in
+            button.tintColor = .gray
+            button.selectedTintColor = .black
+            button.selectedFont = UIFont.systemFont(ofSize: 16, weight: .medium)
         }
+
+        // bar를 안보이게 하고 싶으면 addBar를 지우면 된다. at -> bar 위치
+        addBar(bar, dataSource: self, at: .top)
+    }
 }
 // MARK: - 상단 탭 TM delegate
 extension FlowerPotTabViewController: PageboyViewControllerDataSource, TMBarDataSource {
     func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
         let item = TMBarItem(title: "")
-        item.title = "Page \(index)"
-//        item.image = UIImage(named: "image.png")
-        // ↑↑ 이미지는 이따가 탭바 형식으로 보여줄 때 사용할 것이니 "이미지가 왜 있지?" 하지말고 넘어가주세요.
+        let tabTitle: String = index == 0 ? "획득한 화분" : "미획득 화분"
+        item.title = tabTitle
         
         return item
     }
