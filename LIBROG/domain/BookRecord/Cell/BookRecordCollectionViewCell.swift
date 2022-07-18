1ed1dc2f2ffad99926ba242c998b148b39e0d5eb
theunghee02 <20211150@sungshin.ac.kr>
2022-07-19 03:17:13 +0900

    [feat] BookRecord's UI

---
+++ LIBROG/domain/BookRecord/Cell/BookRecordCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BookRecordCollectionViewCell.swift
+//  LIBROG
+//
+//  Created by 최승희 on 2022/07/19.
+//
+
+import UIKit
+
+class BookRecordCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageBook: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
