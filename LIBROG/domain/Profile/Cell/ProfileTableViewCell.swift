eeb85e6e4f109e0949a8f0849f6d8ef5a0a9c04b
gom1n <alice436777@gmail.com>
2022-07-18 20:02:14 +0900

    [feat] Set Profile page & profile table view

---
+++ LIBROG/domain/Profile/Cell/ProfileTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileTableViewCell.swift
+//  LIBROG
+//
+//  Created by gomin on 2022/07/18.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var menuLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var unitLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    func setProfileMenu(_ imgStr: String, _ menu: String, _ count: Int, _ unit: String) {
+        menuLabel.text = menu
+        countLabel.text = String(count)
+        unitLabel.text = unit
+        iconImageView.image = UIImage(named: imgStr)
+        
+    }
+}
