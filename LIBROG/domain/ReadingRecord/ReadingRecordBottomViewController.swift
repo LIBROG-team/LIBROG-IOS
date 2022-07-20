3e43cad0d3ca3a2bf8cced73114602fc9daac4d9
gom1n <alice436777@gmail.com>
2022-07-20 19:14:50 +0900

    [feat] Change filterButton label when tableView selected & Set delegate and datasource on ReadingRecordVC

--- LIBROG/domain/ReadingRecord/ReadingRecordBottomViewController.swift
+++ LIBROG/domain/ReadingRecord/ReadingRecordBottomViewController.swift
@@ -12,36 +12,15 @@ class ReadingRecordBottomViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        menuTableView.dataSource = self
-        menuTableView.delegate = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
     }
-}
-// MARK: - 독서기록 정렬 메뉴 Table view data source
-extension ReadingRecordBottomViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        if indexPath.row == 0 { cell.textLabel?.text = "최근 기록 순" }
-        else if indexPath.row == 1 { cell.textLabel?.text = "별점 높은 순" }
-        else if indexPath.row == 2 { cell.textLabel?.text = "제목 순" }
-        return cell
-    }
-    //셀 세로 길이 조절
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
-    }
-    //클릭 이벤트 처리
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.dismiss(animated: true, completion: nil)
-        tableView.deselectRow(at: indexPath, animated: true)
+    func setTableViewDataSourceDelegate(dataSourceDelegate: UITableViewDataSource & UITableViewDelegate) {
+        menuTableView.delegate = dataSourceDelegate
+        menuTableView.dataSource = dataSourceDelegate
     }
 }
--- LIBROG/domain/ReadingRecord/ReadingRecordViewController.swift
+++ LIBROG/domain/ReadingRecord/ReadingRecordViewController.swift
@@ -10,6 +10,10 @@ import MaterialComponents.MaterialBottomSheet
 
 class ReadingRecordViewController: UIViewController {
     @IBOutlet weak var readingRecordCollectionView: UICollectionView!
+    
+    @IBOutlet weak var filterButton: UIButton!
+    let filterArray = ["최근 기록 순", "별점 높은 순", "제목 순"]
+    
     @IBAction func scrollToTop_button(_ sender: Any) {
         readingRecordCollectionView.scrollToItem(at: IndexPath(item: -1, section: 0), at: .init(rawValue: 0), animated: true)
     }
@@ -17,6 +21,7 @@ class ReadingRecordViewController: UIViewController {
         let vc = storyboard?.instantiateViewController(withIdentifier: "ReadingRecordBottomVC") as! ReadingRecordBottomViewController
         let bottomSheet: MDCBottomSheetController = MDCBottomSheetController(contentViewController: vc)
         self.present(bottomSheet, animated: true, completion: nil)
+        vc.setTableViewDataSourceDelegate(dataSourceDelegate: self)
 
         // 아래로 드래그해도 안닫히게 하기
         bottomSheet.dismissOnDraggingDownSheet = false
@@ -64,7 +69,7 @@ extension ReadingRecordViewController : UICollectionViewDelegate, UICollectionVi
         return CGSize(width: 94, height: 140)
     }
 }
-// MARK: -  독서기록 정렬필터 선택 후 MDCBottomSheet delegate
+// MARK: -  독서기록 MDCBottomSheet delegate
 extension ReadingRecordViewController: MDCBottomSheetControllerDelegate {
     func bottomSheetControllerDidDismissBottomSheet(_ controller: MDCBottomSheetController) {
         print("바트 시트 닫힘")
@@ -73,5 +78,30 @@ extension ReadingRecordViewController: MDCBottomSheetControllerDelegate {
     func bottomSheetControllerDidChangeYOffset(_ controller: MDCBottomSheetController, yOffset: CGFloat) {
         // 바텀 시트 위치
         print(yOffset)
+    }
+}
+// MARK: - 독서기록 정렬 메뉴 Table view data source
+extension ReadingRecordViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        if indexPath.row == 0 { cell.textLabel?.text = filterArray[0] }
+        else if indexPath.row == 1 { cell.textLabel?.text = filterArray[1] }
+        else if indexPath.row == 2 { cell.textLabel?.text = filterArray[2] }
+        return cell
+    }
+    //셀 세로 길이 조절
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    //클릭 이벤트 처리
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 { filterButton.setTitle(filterArray[0], for: .normal)}
+        else if indexPath.row == 1 {filterButton.setTitle(filterArray[1], for: .normal) }
+        else if indexPath.row == 2 {filterButton.setTitle(filterArray[2], for: .normal)}
+        self.dismiss(animated: true, completion: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
