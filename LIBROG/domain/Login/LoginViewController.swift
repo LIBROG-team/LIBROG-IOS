eb527c3a6d1e4068064ce6318c959a470d831332
gom1n <alice436777@gmail.com>
2022-07-22 00:37:11 +0900

    [feat] Make Login page & viewController

---
+++ LIBROG/domain/Login/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  LIBROG
+//
+//  Created by gomin on 2022/07/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
