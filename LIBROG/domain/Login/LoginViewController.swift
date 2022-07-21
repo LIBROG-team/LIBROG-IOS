887ea7c78a7998b3d3b33dfb14f13ba7fda8c941
gom1n <alice436777@gmail.com>
2022-07-22 00:40:52 +0900

    [feat] Set kakao login button and move to web

--- LIBROG/domain/Login/LoginViewController.swift
+++ LIBROG/domain/Login/LoginViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import KakaoSDKUser
+import KakaoSDKAuth
+import KakaoSDKCommon
 
 class LoginViewController: UIViewController {
 
@@ -15,4 +18,32 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func kakaoLoginButtonDidTap(_ sender: UIButton) {
+        // 카카오톡 설치 여부 확인
+//        if (UserApi.isKakaoTalkLoginAvailable()) {
+//            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+//                if let error = error {
+//                    print(error)
+//                }
+//                else {
+//                    print("loginWithKakaoTalk() success.")
+//
+//                    //do something
+//                    _ = oauthToken
+//                }
+//            }
+//        }
+        // MARK: 카카오톡 로그인 - 웹으로 이동
+        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+           if let error = error {
+             print(error)
+           }
+           else {
+            print("loginWithKakaoAccount() success.")
+            
+            //do something
+            _ = oauthToken
+           }
+        }
+    }
 }
