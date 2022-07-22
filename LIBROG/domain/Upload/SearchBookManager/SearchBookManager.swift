bc1f48c57c313a73e146d427a3829907c2f15b23
gom1n <alice436777@gmail.com>
2022-07-23 02:16:12 +0900

    [fix] Fix korean word search result by encoding - kakao

--- LIBROG/domain/Upload/SearchBookManager/SearchBookManager.swift
+++ LIBROG/domain/Upload/SearchBookManager/SearchBookManager.swift
@@ -12,17 +12,18 @@ import Kingfisher
 class SearchBookManager {
     let header = HeaderMAnager().getHeader()
     func searchBookManager(_ word: String, _ page: Int, _ viewcontroller: UploadViewController) {
-        AF.request("https://dapi.kakao.com/v3/search/book?target=title&size=20&query=\(word)&page=\(page)",
-                   method: .get,
-                   parameters: nil,
-                   headers: header)
+        let originalURL = "https://dapi.kakao.com/v3/search/book?target=title&size=20&query=\(word)&page=\(page)"
+        guard let target = originalURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {return}
+        guard let url = URL(string: target) else {return}
+
+        AF.request(url, method: .get, parameters: nil, headers: header)
             .validate()
             .responseDecodable(of: BookModel.self) { response in
                 
             switch response.result {
             case .success(let result):
                 var book = result.documents
-                print("DEBUG: ", book)
+//                print("DEBUG: ", book)
                 viewcontroller.kakaoSearchBookSuccessAPI(book)
             case .failure(let error):
                 print(error.localizedDescription)
