b6be3a942edab7fd3b5e9aa70c348b6d7bf9d5bd
gom1n <alice436777@gmail.com>
2022-07-26 15:49:39 +0900

    [fix] Unwrap book datas

--- LIBROG/domain/Upload/UploadViewController.swift
+++ LIBROG/domain/Upload/UploadViewController.swift
@@ -153,7 +153,13 @@ extension UploadViewController {
     func kakaoSearchBookSuccessAPI(_ result : KakaoBookModel) {
         let bookData = result.documents
         for book in bookData {
-            dataArray.append(BookData(bookTitle: book.title!, thumbnailURL: book.thumbnail!, author: book.authors, introduction: book.contents!, publisher: book.publisher!, publishedDate: book.datetime!))
+            guard let bookTitle = book.title else {return}
+            guard let thumbnailURL = book.thumbnail else {return}
+            guard let introduction = book.contents else {return}
+            guard let publisher = book.publisher else {return}
+            guard let publishedDate = book.datetime else {return}
+            
+            dataArray.append(BookData(bookTitle: bookTitle, thumbnailURL: thumbnailURL, author: book.authors, introduction: introduction, publisher: publisher, publishedDate: publishedDate))
         }
         self.searchCount = result.meta.pageable_count!
         resultVC.tableView.reloadData()
