b4f28944a4cbe7ac573e45e516112a9aaf03d377
gom1n <alice436777@gmail.com>
2022-07-28 02:06:39 +0900

    [feat] Connect modify record api

---
+++ LIBROG/domain/ModifyRecord/ModifyRecordDataManager/ModifyRecordModel.swift
@@ -0,0 +1,13 @@
+//
+//  ModifyRecordModel.swift
+//  LIBROG
+//
+//  Created by gomin on 2022/07/28.
+//
+
+struct ModifyRecordModel: Decodable {
+    let isSuccess: Bool?
+    let code: Int?
+    let message: String?
+//    let result
+}
