fdea05caa6a2f7c6fc32d7ee5d3c278180d8e416
gom1n <alice436777@gmail.com>
2022-07-28 02:47:18 +0900

    [feat] Make register pages

--- LIBROG/domain/Login/LoginViewController.swift
+++ LIBROG/domain/Login/LoginViewController.swift
@@ -13,14 +13,21 @@ import KakaoSDKCommon
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginTitleLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // '더 편한 전자책 리브로그에 오신 것을 환영합니다'
         guard let text = self.loginTitleLabel.text else { return }
         let attributeString = NSMutableAttributedString(string: text)
         attributeString.addAttribute(.foregroundColor, value: UIColor(named: "LIBROGColor")!, range: (text as NSString).range(of: "리브로그"))
         self.loginTitleLabel.attributedText = attributeString
+        // 로그인 버튼 Custom
+        loginButton.layer.borderColor = UIColor(named: "LIBROGColor")?.cgColor
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.cornerRadius = 20
+        loginButton.tintColor = UIColor(named: "LIBROGColor")
     }
     
     @IBAction func kakaoLoginButtonDidTap(_ sender: UIButton) {
