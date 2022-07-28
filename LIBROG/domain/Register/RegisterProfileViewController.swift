f1c27ae263c8d0376999543ed9e0766e1f1e46ff
theunghee02 <20211150@sungshin.ac.kr>
2022-07-28 16:43:07 +0900

    [feat] Add RegisterProfile's bottom Sheet

--- LIBROG/domain/Register/RegisterProfileViewController.swift
+++ LIBROG/domain/Register/RegisterProfileViewController.swift
@@ -6,11 +6,29 @@
 //
 
 import UIKit
+import MaterialComponents.MaterialBottomSheet
 
 class RegisterProfileViewController: UIViewController {
 
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var cameraButton: UIButton!
+    let selectArray = ["앨범에서 사진/동영상 선택", "리브로그 프로필로 설정", "기본 이미지로 변경"]
+    
+    @IBAction func cameraDidTapButton(_ sender: UIButton) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "RegisterProfileBottomVC") as! RegisterProfileBottomViewController
+        let bottomSheet: MDCBottomSheetController = MDCBottomSheetController(contentViewController: vc)
+        self.present(bottomSheet, animated: true, completion: nil)
+        vc.setTableViewDataSourceDelegate(dataSourceDelegate: self)
+
+        // 아래로 드래그해도 안닫히게 하기
+        bottomSheet.dismissOnDraggingDownSheet = false
+        // 높이
+        bottomSheet.mdc_bottomSheetPresentationController?.preferredSheetHeight = 197
+        // 뒤에 배경 컬러
+        bottomSheet.scrimColor = UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 0.54)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,5 +45,58 @@ class RegisterProfileViewController: UIViewController {
     }
 
     @IBAction func registerButtonDidTap(_ sender: UIButton) {
+    }
+}
+
+// MARK: -  회원가입 사진추가 MDCBottomSheet delegate
+extension RegisterProfileViewController: MDCBottomSheetControllerDelegate {
+    func bottomSheetControllerDidDismissBottomSheet(_ controller: MDCBottomSheetController) {
+        print("바트 시트 닫힘")
+    }
+    
+    func bottomSheetControllerDidChangeYOffset(_ controller: MDCBottomSheetController, yOffset: CGFloat) {
+        // 바텀 시트 위치
+        print(yOffset)
+    }
+}
+
+// MARK: - 독서기록 정렬 메뉴 Table view data source
+extension RegisterProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.contentInsetAdjustmentBehavior = .never
+        let cell = UITableViewCell()
+        cell.textLabel?.font = UIFont(name: "Apple SD Gothic Neo", size: 14)
+        if indexPath.row == 0 { cell.textLabel?.text = selectArray[0] }
+        else if indexPath.row == 1 { cell.textLabel?.text = selectArray[1] }
+        else if indexPath.row == 2 { cell.textLabel?.text = selectArray[2] }
+        return cell
+    }
+    //셀 세로 길이 조절
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    //클릭 이벤트 처리
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 버튼 config 추가
+        var config = UIButton.Configuration.plain()
+        var attText = AttributedString.init(selectArray[0])
+        
+        if indexPath.row == 0 { attText = AttributedString.init(selectArray[0])}
+        else if indexPath.row == 1 {attText = AttributedString.init(selectArray[1]) }
+        else if indexPath.row == 2 {attText = AttributedString.init(selectArray[2])}
+        
+        // config font & image 설정
+        attText.font = UIFont.systemFont(ofSize: 13.0, weight: .regular)
+        config.attributedTitle = attText
+        config.image = UIImage.init(named: "arrow-down")
+        config.imagePlacement = .leading
+        config.imagePadding = 5
+        cameraButton.configuration = config
+        
+        self.dismiss(animated: true, completion: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
